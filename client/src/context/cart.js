04dceb3f2bb22e521bc06813045920c071797a96
Rasulwebs <boboqulovrasul80@gmail.com>
2023-06-29 22:30:38 +0500

    editing main and context

--- client/src/context/cart.js
+++ client/src/context/cart.js
@@ -5,10 +5,10 @@ const CartContext = createContext();
 const CartProvider = ({ children }) => {
   const [cart, setCart] = useState([]);
 
-  useEffect(()=>{
-    let existingCartItem=localStorage.getItem("cart")
-    if(existingCartItem) setCart(JSON.parse(existingCartItem))
-  },[])
+  useEffect(() => {
+    let existingCartItem = localStorage.getItem("cart");
+    if (existingCartItem) setCart(JSON.parse(existingCartItem));
+  }, []);
   return (
     <CartContext.Provider value={[cart, setCart]}>
       {children}
